157958b2ca7631437804fa511960bb174a67f808
Denise Santos <denisesantosdev@gmail.com>
2024-10-18 16:24:30 -0300

    complete create account page

--- src/app/pages/auth-page/auth-page.component.ts
+++ src/app/pages/auth-page/auth-page.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-auth-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, InputTextModule, PasswordModule, FormsModule,FloatLabelModule],
   templateUrl: './auth-page.component.html',
-  styleUrl: './auth-page.component.scss'
+  styleUrl: './auth-page.component.scss',
 })
-export class AuthPageComponent {
-
-}
+export class AuthPageComponent {}
